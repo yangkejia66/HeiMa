7808e68ec8f0bc00c3eb7b8d960a3683a3e7e794
yangkejia66 <yangkejia66@foxmail.com>
2019-07-09 11:04:22 +0800

    使用Element-UI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入ElementUI
+import ElementUI from 'element-ui'
+// 引入Element样式
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App)
 }).$mount('#app')
