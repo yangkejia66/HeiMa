ed3eb0829064c7e1cd40114709a9009bf4cbecc7
yangkejia66 <yangkejia66@foxmail.com>
2019-07-09 11:28:23 +0800

    使用 Vue-router

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import ElementUI from 'element-ui'
 // 引入Element样式
 import 'element-ui/lib/theme-chalk/index.css'
 
+import router from '@/router'
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+// 引入Vue
+import Vue from 'vue'
+// 引入Vue-Router
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: []
+})
+
+export default {
+  router
+}
